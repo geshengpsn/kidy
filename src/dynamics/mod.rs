78eeb88812ba49593f176ec6054e2c6a1cc49c17
geshengpsn <gesheng199832@126.com>
2024-11-28 21:07:03 +0800

    update

--- src/dynamics/mod.rs
+++ src/dynamics/mod.rs
@@ -1,10 +1,10 @@
 use crate::KidyChain;
-use liealg::{prelude::*, se3, AdjSE3, Vec6};
-use nalgebra::{Matrix3, Matrix6, Vector3, Vector6};
+use liealg::{prelude::*, se3};
+use nalgebra::{Matrix6, Vector3, Vector6};
 
 impl<const N: usize> KidyChain<N> {
     // tau = M(q) * q_dot_dot + C(q, q_dot) * q_dot + G(q)
-    fn id(
+    pub fn id(
         &self,
         theta: [f64; N],
         dtheta: [f64; N],
@@ -32,31 +32,32 @@ impl<const N: usize> KidyChain<N> {
 
             // dV_i: acceleration of link i
             let dv = t.adjoint().act(&dtwists[i - 1])
-                + bracket(v, self.local_screw[i - 1].clone()) * dtheta[i - 1]
+                + ad_mul_se(ad(&v), &self.local_screw[i - 1]) * dtheta[i - 1]
                 + self.local_screw[i - 1].clone() * ddtheta[i - 1];
             dtwists[i] = dv;
         }
 
         let mut wrenchs = vec![se3::new([0.0, 0.0, 0.0], [0.0, 0.0, 0.0]); N + 1];
         wrenchs[N] = f_tip;
-        let tau = [0.0; N];
+        let mut tau = [0.0; N];
         for i in (1..N + 1).rev() {
             let t = (self.local_screw[i].clone() * -theta[i - 1]).exp()
                 * self.local_zero_pose[i + 1].clone().inv();
-            // t.adjoint().transpose().act(&wrenchs[i + 1]) + inertial_mul_dtwist(g, v) - ad(v).transpose().;
-            // wrenchs[i] = wrenchs[i + 1] +
-            // tau[i - 1] =
+            wrenchs[i] = t.adjoint().transpose().act(&wrenchs[i + 1])
+                + inertial_mul_se3(self.local_spatial_inertial[i], &dtwists[i])
+                - ad_mul_se(ad(&twists[i]).transpose(), &twists[i]);
+            tau[i - 1] = cross_se3(&wrenchs[i], &self.local_screw[i - 1]);
         }
         tau
     }
 
     // M(q)^-1 * (tau - C(q, q_dot) * q_dot - G(q)) = q_dot_dot
-    fn fd(&self) {
+    pub fn fd(&self) {
         println!("KidyChain fd");
     }
 }
 
-fn ad(v: se3<f64>) -> Matrix6<f64> {
+fn ad(v: &se3<f64>) -> Matrix6<f64> {
     let v = v.vee().as_array();
     let p = Vector3::new(v[0], v[1], v[2]);
     let w = Vector3::new(v[3], v[4], v[5]);
@@ -69,33 +70,32 @@ fn ad(v: se3<f64>) -> Matrix6<f64> {
     res
 }
 
-fn bracket(v: se3<f64>, w: se3<f64>) -> se3<f64> {
-    let m = ad(v);
+fn ad_mul_se(m: Matrix6<f64>, w: &se3<f64>) -> se3<f64> {
     let vec6 = Vector6::from_column_slice(w.vee().as_slice());
     let res = m * vec6;
     se3::new([res[0], res[1], res[2]], [res[3], res[4], res[5]])
 }
 
-fn inertial_mul_twist(g: Matrix6<f64>, v: se3<f64>) -> se3<f64> {
+fn inertial_mul_se3(g: Matrix6<f64>, v: &se3<f64>) -> se3<f64> {
     let vec6 = Vector6::from_column_slice(v.vee().as_slice());
     let res = g * vec6;
     se3::new([res[0], res[1], res[2]], [res[3], res[4], res[5]])
 }
 
-fn inertial_mul_dtwist(g: Matrix6<f64>, dv: se3<f64>) -> se3<f64> {
-    let vec6 = Vector6::from_column_slice(dv.vee().as_slice());
-    let res = g * vec6;
-    se3::new([res[0], res[1], res[2]], [res[3], res[4], res[5]])
+fn cross_se3(v: &se3<f64>, w: &se3<f64>) -> f64 {
+    let vec_v = Vector6::from_column_slice(v.vee().as_slice());
+    let vec_w = Vector6::from_column_slice(w.vee().as_slice());
+    vec_v.dot(&vec_w)
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+    // use super::*;
 
     #[test]
     fn test_ad() {
-        let v = se3::new([1.0, 2.0, 3.0], [4.0, 5.0, 6.0]);
-        let res = ad(v);
-        println!("{}", res);
+        // let v = se3::new([1.0, 2.0, 3.0], [4.0, 5.0, 6.0]);
+        // let res = ad(v);
+        // println!("{}", res);
     }
 }
